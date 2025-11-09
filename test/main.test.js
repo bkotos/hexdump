4c8f40a6286b542f549002a8e87dc86d8f7f5586
Brian Kotos <brian.kotos@gmail.com>
2025-11-08 23:12:53 -0500

    Update cwd and also move png input to test/data directory.

--- test/main.test.js
+++ test/main.test.js
@@ -7,9 +7,9 @@ const getFileContent = (path) => {
   return readFileSync(join(process.cwd(), path), 'utf-8')
 }
 
-const runCommand = (command, args, options) => {
+const runCommand = (command, args) => {
   const result = spawnSync(command, args, {
-    ...options,
+    cwd: process.cwd(),
     stdio: ['ignore', 'pipe', 'pipe'], // ignore stdin, capture stdout and stderr
     encoding: 'utf-8',
   })
@@ -20,13 +20,10 @@ const runCommand = (command, args, options) => {
 describe('Hexdump PHP Tests', () => {
   it('should produce correct hexdump output for test.png', () => {
     // arrange
-    const phpDir = join(process.cwd(), 'php')
     const expectedOutput = getFileContent('test/data/output1-bin.txt')
     
     // act
-    const stdout = runCommand('php', ['hexdump.php', 'test.png'], {
-      cwd: phpDir,
-    })
+    const stdout = runCommand('php', ['php/hexdump.php', 'test/data/input1.png'])
     
     // assert
     expect(stdout).toBe(expectedOutput)
