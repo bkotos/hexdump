cfe43b5c4264bcb6590f6ab2b7737a4e52db4741
Brian Kotos <brian.kotos@gmail.com>
2025-11-10 19:22:28 -0500

    Progress on Go implementation.

--- go/main.go
+++ go/main.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-}
+	// fmt.Println("Hello, World!")
 
+	data, _ := os.ReadFile("test/data/input1.png")
+	// fmt.Println(string(data))
+
+	// line := ""
+	for _, b := range data {
+		// fmt.Printf("00000000: Byte %d: %d (char: %c)\n", i, b, b)
+		// line := line + "00000000: [" + string(b) + "]"
+		// fmt.Print(line)
+		// fmt.Print(string(b))
+
+		printable := string(byte(46))
+		if b >= 32 && b <= 126 {
+			printable = string(b)
+		}
+
+		fmt.Print(printable)
+
+		// fmt.Printf("00000000: %x (char: %c)\n", b, b)
+		// break
+	}
+}
