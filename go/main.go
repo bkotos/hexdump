fa0b64b67325e16707944ccecba92f8c9f4746f8
Brian Kotos <brian.kotos@gmail.com>
2025-11-10 20:49:53 -0500

    Progress

--- go/main.go
+++ go/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,18 +13,47 @@ func main() {
 	// fmt.Println(string(data))
 
 	// line := ""
-	for _, b := range data {
-		// fmt.Printf("00000000: Byte %d: %d (char: %c)\n", i, b, b)
-		// line := line + "00000000: [" + string(b) + "]"
-		// fmt.Print(line)
-		// fmt.Print(string(b))
+	var bytes []string
+	chars := ""
 
-		printable := getPrintableChar(b)
-		fmt.Print(printable)
+	bytesPerLine := 16
+	colsPerByte := 3
 
-		// fmt.Printf("00000000: %x (char: %c)\n", b, b)
-		// break
+	byteOffset := 0
+	line := ""
+	for i, b := range data {
+		isFirstByteOfLine := i%bytesPerLine == 0
+		if isFirstByteOfLine {
+			line = fmt.Sprintf("%08x", byteOffset)
+		}
+
+		bytes = append(bytes, fmt.Sprintf("%02x", b))
+		chars = chars + getPrintableChar(b)
+
+		isNewLine := (i+1)%bytesPerLine == 0
+		if isNewLine {
+			fmt.Print(renderDump(bytesPerLine, colsPerByte, line, &bytes, &chars))
+			line = ""
+		}
+
+		byteOffset++
 	}
+
+	if len(bytes) > 0 {
+		fmt.Print(renderDump(bytesPerLine, colsPerByte, line, &bytes, &chars))
+	}
+}
+
+func renderDump(bytesPerLine int, colsPerByte int, line string, bytes *[]string, chars *string) string {
+	x := strings.Join(*bytes, " ")
+	width := bytesPerLine * colsPerByte
+	byteDump := fmt.Sprintf("%-*s", width, x)
+
+	dump := line + ": " + byteDump + " " + *chars + "\n"
+	*bytes = []string{}
+	*chars = ""
+
+	return dump
 }
 
 func getPrintableChar(b byte) string {
